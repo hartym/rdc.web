168d010803f01544c5e84ad8dfcefe57f4ad6651
Romain Dorgueil <romain@dorgueil.net>
2014-01-22 10:47:13 +0100

    server can take an application, or not, when imported as a dic module.

--- rdc/web/server.py
+++ rdc/web/server.py
@@ -1,4 +1,3 @@
-from functools import partial
 from rdc.dic import Container as BaseContainer
 
 class Container(BaseContainer):
@@ -9,21 +8,29 @@ class Container(BaseContainer):
         p = lambda name: '.'.join((prefix, name, ))
         r = lambda name: self.ref(p(name[1:]) if name[0] is '.' else name)
 
+        server_kwargs = {
+            'hostname': kwargs.pop('bind', None) or r('.server.bind'),
+            'port': kwargs.pop('port', None) or r('.server.port'),
+            'use_debugger': kwargs.pop('debug', None) or r('.server.debug'),
+            'use_reloader': kwargs.pop('reload', None) or r('.server.reload'),
+        }
+        if 'application' in kwargs:
+            server_kwargs['application'] = kwargs['application']
+
         #
         # Embedded development web server
         #
-        @self.definition(
-            prefix,
-            kwargs.pop('bind', None) or r('.server.bind'),
-            kwargs.pop('port', None) or r('.server.port'),
-            use_debugger = kwargs.pop('debug', None) or r('.server.debug'),
-            use_reloader = kwargs.pop('reload', None) or r('.server.reload'),
-        )
-        def server(hostname, port, use_reloader, use_debugger):
+        @self.definition(prefix, **server_kwargs)
+        def server(**kwargs):
             try:
                 from werkzeug.serving import run_simple
             except ImportError as e:
                 raise ImportError(
                     'You must install werkzeug to use the embedded development webserver (try "pip install werkzeug").'
                 )
-            return partial(run_simple, hostname, port, use_reloader=use_reloader, use_debugger=use_debugger)
+
+            def server(*lastminute_args, **lastminute_kwargs):
+                kwargs.update(lastminute_kwargs)
+                return run_simple(**kwargs)
+
+            return server
